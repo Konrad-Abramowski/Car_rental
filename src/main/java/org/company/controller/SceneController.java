1261189a65c65dfa71df671247cbeeca6dac5e3d
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-12-30 01:09:35 +0100

    Create SceneController for switching Scenes etc.

--- src/main/java/org/company/controller/SceneController.java
+++ src/main/java/org/company/controller/SceneController.java
@@ -1,4 +1,32 @@
 package org.company.controller;
 
-class SceneController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class SceneController {
+    public static Scene getSceneFromFxml(String fxml) throws IOException {
+        String path = "src/main/resources/view/" + fxml +".fxml";
+        FXMLLoader loader = new FXMLLoader();
+        FileInputStream loginFxml =
+                new FileInputStream(new File(path));
+        Parent root = loader.load(loginFxml);
+        return  new Scene(root);
+    }
+
+    public static void switchScenes(ActionEvent event, String fxml) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        //stage.setMaximized(true);
+        Scene scene = SceneController.getSceneFromFxml(fxml);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
