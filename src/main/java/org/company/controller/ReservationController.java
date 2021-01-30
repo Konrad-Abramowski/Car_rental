d7939be8aaeb9e6135d1a805eabb6bd96d3c0431
Artur Sołtys <arturac2@gmail.com>
2021-01-31 00:41:46 +0100

    Update ReservationController class

--- src/main/java/org/company/controller/ReservationController.java
+++ src/main/java/org/company/controller/ReservationController.java
@@ -7,6 +7,10 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import org.company.App;
 import org.company.model.Car;
+import org.company.model.Client;
+import org.company.model.Loan;
+
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.io.IOException;
 import java.time.Instant;
@@ -33,29 +37,49 @@ public class ReservationController {
     private Button btnCalculateCost;
 
     long daysOfLoan;
-
+    LocalDate startLoanDate;
+    LocalDate endLoanDate;
     @FXML
     void btnCalculateCostHandler(ActionEvent event) {
-        LocalDate startDate = startDatePicker.getValue();
-        LocalDate endDate = endDatePicker.getValue();
-        if (LocalDate.now().compareTo(startDate)>0 ){
+        startLoanDate = startDatePicker.getValue();
+        endLoanDate = endDatePicker.getValue();
+        if (LocalDate.now().compareTo(startLoanDate)>0 ){
             displayAllert("Incorrect dates!");
         }
-        else if(startDate.compareTo(endDate) >= 0){
-            displayAllert("Start date cannot be after end Date!");
+        else if(startLoanDate.compareTo(endLoanDate) >= 0){
+            displayAllert("Incorrect dates!");
         }
         else {
-            daysOfLoan = ChronoUnit.DAYS.between(startDate, endDate);
+            daysOfLoan = ChronoUnit.DAYS.between(startLoanDate, endLoanDate);
             float totalCost = calculateReservationCost(ShoppingCartController.carsList);
             totalCostTextField.setText(Float.toString(totalCost));
         }
     }
 
-
     @FXML
     void btnConfirmReservationHandler(ActionEvent event) throws IOException {
-        SceneController.switchScenes(event,"client_panel_shopping_cart");
+        if (!totalCostTextField.getText().isEmpty()) {
+            LocalDateTime localStartDateTime = startLoanDate.atStartOfDay();
+            LocalDateTime localEndDateTime = startLoanDate.atStartOfDay();
+
+            for (Car car : ShoppingCartController.carsList) {
+                float price = car.getPrice() * daysOfLoan;
+                Client client = App.clientDao.get(App.activeUserId).get();
+                Loan loan = new Loan(localStartDateTime, localEndDateTime, price, client, car);
+                App.loanDao.create(loan);
+            }
+            ShoppingCartController.carsList.clear();
+
+            Alert errorAlert = new Alert(Alert.AlertType.INFORMATION);
+            errorAlert.setHeaderText("You have confirmed the reservation successfully!");
+            errorAlert.showAndWait();
+            SceneController.switchScenes(event, "client_panel_shopping_cart");
+        }
+        else {
+            displayAllert("You have to calculate cost first");
+        }
     }
+
     private void  displayAllert(String allertText){
         Alert errorAlert = new Alert(Alert.AlertType.ERROR);
         errorAlert.setHeaderText(allertText);
