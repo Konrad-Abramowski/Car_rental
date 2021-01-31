641d61e5a4aa42e520816be5b8bf2c59ec1a0e1a
Abramowski-Konrad <abramowski.konrad@gmail.com>
2021-01-31 02:56:10 +0100

    Handle incorrect data type while adding car

--- src/main/java/org/company/controller/AddCarController.java
+++ src/main/java/org/company/controller/AddCarController.java
@@ -3,6 +3,7 @@ package org.company.controller;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 import org.company.App;
@@ -62,21 +63,65 @@ public class AddCarController {
             }
             if (mileage.isEmpty()) {
                 mileageTF.setPromptText("Car's mileage must not be empty!");
+            }else{
+                try{
+                    Float.parseFloat(mileage);
+
+                } catch (NumberFormatException e){
+                    Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                    errorAlert.setHeaderText("Error");
+                    errorAlert.setContentText("Incorrect mileage data type (not float)");
+                    errorAlert.showAndWait();
+                    return;
+                }
             }
             if (countryOfOrigin.isEmpty()) {
                 countryOfOriginTF.setPromptText("Car's country of origin must not be empty!");
             }
             if (productionYear.isEmpty()) {
                 productionYearTF.setPromptText("Car's production year name must not be empty!");
+            }else{
+                try{
+                    Float.parseFloat(productionYear);
+
+                } catch (NumberFormatException e){
+                    Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                    errorAlert.setHeaderText("Error");
+                    errorAlert.setContentText("Incorrect production year data type (not integer)");
+                    errorAlert.showAndWait();
+                    return;
+                }
             }
             if (vin.isEmpty()) {
                 vinTF.setPromptText("Car's vin must not be empty!");
             }
             if (engineCapacity.isEmpty()) {
                 engineCapacityTF.setPromptText("Car's engine capacity must not be empty!");
+            } else{
+                try{
+                    Float.parseFloat(engineCapacity);
+
+                } catch (NumberFormatException e){
+                    Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                    errorAlert.setHeaderText("Error");
+                    errorAlert.setContentText("Incorrect engine capacity data type (not float)");
+                    errorAlert.showAndWait();
+                    return;
+                }
             }
             if (price.isEmpty()) {
                 priceTF.setPromptText("Car's price must not be empty!");
+            }else{
+                try{
+                    Float.parseFloat(price);
+
+                } catch (NumberFormatException e){
+                    Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                    errorAlert.setHeaderText("Error");
+                    errorAlert.setContentText("Incorrect price data type (not integer)");
+                    errorAlert.showAndWait();
+                    return;
+                }
             }
             if (color.isEmpty()) {
                 colorTF.setPromptText("Car's color must not be empty!");
